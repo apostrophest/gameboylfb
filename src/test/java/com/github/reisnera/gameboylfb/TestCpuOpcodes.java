83cf4504798b40c4c5d5826e9197c3f9c78ba70e
Alex Reisner <thearcher@gmail.com>
2015-10-08 20:51:25 -0400

    More work on cpu opcode testing

--- src/test/java/com/github/reisnera/gameboylfb/TestCpuOpcodes.java
+++ src/test/java/com/github/reisnera/gameboylfb/TestCpuOpcodes.java
@@ -9,7 +9,7 @@
 package com.github.reisnera.gameboylfb;
 
 import org.testng.annotations.*;
-//import static org.testng.Assert.*;
+import static org.testng.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class TestCpuOpcodes {
@@ -81,10 +81,27 @@ public class TestCpuOpcodes {
         verifyZeroInteractions(mem);
     }
 
-    // INC B
+    // INC B - results in zero and half-carry
     @Test
-    public void testOpcode04() {
-        // TODO: this test needs to test flags!
+    public void testOpcode04a() {
+        // prepare
+        cpu.reg.setB(0xFF);
+        reset(cpu.reg);
+        // test
+        cpu.processOpcode(0x04);
+        // verify
+        verify(cpu.reg, atLeastOnce()).getB();
+        verify(cpu.reg).setB(0xFF + 1);
+        verify(cpu.reg).clearFlagN();
+        verify(cpu.reg).setFlagZ();
+        verify(cpu.reg).setFlagH();
+        verifyNoMoreInteractions(cpu.reg);
+        verifyZeroInteractions(mem);
+    }
+
+    // INC B - results in no flags
+    @Test
+    public void testOpcode04b() {
         // prepare
         cpu.reg.setB(TEST_BYTE);
         reset(cpu.reg);
@@ -94,24 +111,59 @@ public class TestCpuOpcodes {
         verify(cpu.reg, atLeastOnce()).getB();
         verify(cpu.reg).setB(TEST_BYTE + 1);
         verify(cpu.reg).clearFlagN();
+        verify(cpu.reg).clearFlagZ();
+        verify(cpu.reg).clearFlagH();
         verifyNoMoreInteractions(cpu.reg);
         verifyZeroInteractions(mem);
     }
 
-    // DEC B
+    // DEC B - results in half-carry
     @Test
-    public void testOpcode05() {
-        // TODO: this test needs to test flags!
+    public void testOpcode05a() {
         // prepare
-        cpu.reg.setB(TEST_BYTE);
+        cpu.reg.setB(0);
         reset(cpu.reg);
         // test
         cpu.processOpcode(0x05);
         // verify
         verify(cpu.reg, atLeastOnce()).getB();
-        verify(cpu.reg).setB(TEST_BYTE - 1);
+        verify(cpu.reg).setB(0 - 1);
         verify(cpu.reg).setFlagN();
+        verify(cpu.reg).setFlagH();
+        verify(cpu.reg).clearFlagZ();
         verifyNoMoreInteractions(cpu.reg);
         verifyZeroInteractions(mem);
+    }
+
+    // DEC B - results in zero
+    @Test
+    public void testOpcode05b() {
+        // prepare
+        cpu.reg.setB(1);
+        reset(cpu.reg);
+        // test
+        cpu.processOpcode(0x05);
+        // verify
+        verify(cpu.reg, atLeastOnce()).getB();
+        verify(cpu.reg).setB(0);
+        verify(cpu.reg).setFlagN();
+        verify(cpu.reg).setFlagZ();
+        verify(cpu.reg).clearFlagH();
+        verifyNoMoreInteractions(cpu.reg);
+        verifyZeroInteractions(mem);
+    }
+
+    // LD B,d8
+    @Test
+    public void testOpcode06() {
+        // prepare
+        when(mem.readByte(anyInt())).thenReturn(TEST_BYTE);
+        // test
+        cpu.processOpcode(0x06);
+        // verify
+        verify(mem).readByte(anyInt());
+        verify(cpu.reg).getThenIncPC();
+        verify(cpu.reg).setB(TEST_BYTE);
+        verifyNoMoreInteractions(mem, cpu.reg);
     }
 }
